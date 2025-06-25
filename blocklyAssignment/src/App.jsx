f5e0b939e9aa18573afec7990437f6c93604578d
pilli05 <pillirajesh@ymail.com>
2025-06-25 17:32:08 +0530

    initial setup

---
+++ blocklyAssignment/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-danger">Hello Bootstrap in Vite + React!</h1>
+      <button className="btn btn-success">Click Me</button>
+    </>
+  );
+}
+
+export default App;
